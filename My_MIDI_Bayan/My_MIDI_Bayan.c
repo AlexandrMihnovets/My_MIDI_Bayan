39ced6f27c8f5fb9d75e4b0b7ebdc9d510dcc86b
Alexandr <sasha_mihnovets@mail.ru>
2022-01-24 14:37:57 +0500

    First commit

---
+++ My_MIDI_Bayan/My_MIDI_Bayan.c
@@ -0,0 +1,16 @@
+/*
+ * My_MIDI_Bayan.c
+ * Created: 24.01.2022 14:32:22
+ * Author: MikhnovetsA
+ */ 
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
